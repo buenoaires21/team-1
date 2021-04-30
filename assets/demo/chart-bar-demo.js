a9b49b12cfbc462f1642e833baf2aed180c44a31
Agustin Cabral <agustincabralmartinez@gmail.com>
2021-04-30 15:27:38 -0300

    Actualizacion graficos pagina

--- assets/demo/chart-bar-demo.js
+++ assets/demo/chart-bar-demo.js
@@ -7,12 +7,12 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["January", "February", "March", "April", "May", "June"], // Se deben agregar los elementos de la base de datos
     datasets: [{
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [90, 10, 20, 40 , 50, 85, 20],
     }],
   },
   options: {
@@ -31,7 +31,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 100,
           maxTicksLimit: 5
         },
         gridLines: {
